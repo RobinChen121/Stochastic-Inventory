0b77e42516c0d141d9f4c5c136c9d0f7b40ef096
RobinChen121 <40953071+RobinChen121@users.noreply.github.com>
2023-07-24 12:52:42 +0800

    update

--- src/leadtime/Leadtime.java
+++ src/leadtime/Leadtime.java
@@ -1,5 +1,16 @@
 package leadtime;
 
+import java.lang.Thread.State;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+
+import sdp.inventory.GetPmf;
+import sdp.inventory.LeadtimeState;
+import sdp.inventory.ImmediateValue.ImmediateValueFunction;
+import sdp.inventory.StateTransition.StateTransitionFunction;
+import umontreal.ssj.probdist.Distribution;
+import umontreal.ssj.probdist.PoissonDist;
+
 /**
 *@author: zhenchen
 *@date: Jul 23, 2023, 10:09:59 AM
@@ -10,7 +21,61 @@ package leadtime;
 public class Leadtime {
 
 	public static void main(String[] args) {
-		
+		double initialInventory = 0; 
+	      double[] meanDemand = {10, 10};
+	      
+	      double truncationQuantile = 0.9999;  
+	      double stepSize = 1; 
+	      double minInventory = -150;
+	      double maxInventory = 300;
+	      int T = meanDemand.length;
+
+	      double fixedOrderingCost = 0; 
+	      double proportionalOrderingCost = 1; 
+	      double holdingCost = 2;
+	      double penaltyCost = 10;
+	      
+	      int maxOrderQuantity = 200;
+	      
+	      Distribution[] distributions = IntStream.iterate(0, i -> i + 1)
+	                                              .limit(T)
+	                                              .mapToObj(i -> new PoissonDist(meanDemand[i]))
+	                                              //.mapToObj(i -> new NormalDist(meanDemand[i], 0.25 * meanDemand[i]))
+	                                              .toArray(Distribution[]::new); // replace for loop
+	      double[][][] pmf = new GetPmf(distributions, truncationQuantile, stepSize).getpmf();
+	      
+	   // feasible actions
+		Function<LeadtimeState, double[]> getFeasibleAction = s -> {
+			double[] feasibleActions = new double[(int) (maxOrderQuantity / stepSize) + 1];
+			int index = 0;
+			for (double i = 0; i <= maxOrderQuantity; i = i + stepSize) {
+				feasibleActions[index] = i;
+				index++;
+			}
+			return feasibleActions;
+		};
+
+		// state transition function
+		StateTransitionFunction<LeadtimeState, Double, Double, LeadtimeState> stateTransition = (state, action, randomDemand) -> {
+			double nextPreQ = action;
+					state.getIniInventory() + action - randomDemand;
+			nextInventory = nextInventory > maxInventory ? maxInventory : nextInventory;
+			nextInventory = nextInventory < minInventory ? minInventory : nextInventory;
+			return new State(state.getPeriod() + 1, nextInventory);
+		};
+
+		// immediate value
+		ImmediateValueFunction<State, Double, Double, Double> immediateValue = (state, action, randomDemand) -> {
+			double fixedCost = 0, variableCost = 0, inventoryLevel = 0, holdingCosts = 0, penaltyCosts = 0;
+			fixedCost = action > 0 ? fixedOrderingCost : 0;
+			variableCost = variOrderingCost * action;
+			inventoryLevel = state.getIniInventory() + action - randomDemand;
+			holdingCosts = holdingCost * Math.max(inventoryLevel, 0);
+			penaltyCosts = penaltyCost * Math.max(-inventoryLevel, 0);
+			double totalCosts = fixedCost + variableCost + holdingCosts + penaltyCosts;
+			return totalCosts;
+		};
+	      
 
 	}
 
--- src/sdp/inventory/LeadtimeState.java
+++ src/sdp/inventory/LeadtimeState.java
@@ -1,5 +1,4 @@
 package sdp.inventory;
-import sdp.inventory.State;
 
 /**
 *@author: zhenchen
@@ -9,12 +8,50 @@ import sdp.inventory.State;
 */
 
 public class LeadtimeState extends State{
-	double preInventory;
+	double preQ;
 	
-	public LeadtimeState(int period, double initialInventory, double preInventory) {
+	public LeadtimeState(int period, double initialInventory, double preQ) {
 		super(period, initialInventory);
-		this.preInventory = preInventory;
+		this.preQ = preQ;
 	}
+	
+	public double getPreQ() {
+		return this.preQ;
+	}
+	
+	@Override
+	public int hashCode(){
+		String hash = "";
+		hash = hash + period + initialInventory + preQ;
+		return hash.hashCode();
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof LeadtimeState)
+			return ((LeadtimeState) o).period == this.period &&
+					((LeadtimeState) o).initialInventory == this.initialInventory &&
+							((LeadtimeState) o).preQ == this.preQ;
+		else
+			return false;
+	}
+	
+	@Override
+	public String toString() {
+		return "period = " + period + ", " + "initialInventory = " + initialInventory + ", " + "preQ = " + preQ;
+	}
+	
+	
+	public int compareTo(LeadtimeState o) {
+		return this.period > o.period ? 1
+				: this.period == o.period
+						? this.initialInventory > o.initialInventory ? 1
+								: this.initialInventory == o.initialInventory ? 
+										this.preQ > o.preQ ?  1:
+											(this.preQ == o.preQ ? 0 : -1):
+												-1:-1;
+	}
+		
 
 }
 
